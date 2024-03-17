2729a6375afb20410889d62ddc0bf1e7f1518407
Anastasiia Kotliarenko <anastasiia.kotliarenko@ukma.edu.ua>
2024-03-17 22:00:19 +0100

    updated path of files

--- main.py
+++ main.py
@@ -5,9 +5,9 @@ import app.io.output as my_output
 def main():
     text_from_console = my_input.input_text_from_console()
 
-    file_content_native = my_input.read_from_file_native('input.txt')
+    file_content_native = my_input.read_from_file_native('data/input.txt')
 
-    file_content_pandas = my_input.read_from_file_with_pandas('input.csv')
+    file_content_pandas = my_input.read_from_file_with_pandas('data/input.csv')
 
     files = [text_from_console, file_content_native, str(file_content_pandas)]
 
@@ -15,7 +15,7 @@ def main():
         my_output.output_text_to_console(file)
 
     for file in files:
-        my_output.write_to_file_native('output.txt', file)
+        my_output.write_to_file_native('data/output.txt', file)
 
 
 if __name__ == "__main__":
