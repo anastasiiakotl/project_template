bc77effd52cf9c3fe15eac265999515078929907
Anastasiia Kotliarenko <anastasiia.kotliarenko@ukma.edu.ua>
2024-03-17 17:37:38 +0100

    with empty input and output functions

---
+++ app/io/input.py
@@ -0,0 +1,10 @@
+def input_text_from_console():
+    pass
+
+
+def read_from_file_native(file_path):
+    pass
+
+
+def read_from_file_with_pandas(file_path):
+    pass
---
+++ app/io/output.py
@@ -0,0 +1,6 @@
+def output_text_to_console(text):
+    pass
+
+
+def write_to_file_native(file_path, content):
+    pass
